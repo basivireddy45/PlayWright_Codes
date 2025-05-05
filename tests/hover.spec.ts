92e85f6512243e8875d02641510aa76c64c59c3c
basivireddy45 <2301050106cse@gmail.com>
2025-05-05 07:48:02 +0530

    playtest

--- tests/hover.spec.ts
+++ tests/hover.spec.ts
@@ -1,12 +1,11 @@
+
 import { chromium,test,Page } from "@playwright/test";
-//const{ chromium,test,expect,page}= require('@playwright/test');
+
 test.describe("login test",async () =>{
       test("verify login page",async ({page})=>{
       
       await page.goto("https://ecommerce-playground.lambdatest.io/index.php?route=information/information&information_id=4");
-     // await page.fill("[name='username']","2301050106");
-     // await page.fill("[name='password']","4512Kbr@");
-      //await page.click("[type='submit']");
+     
        await page.hover("//span[normalize-space()='Mega Menu']");
        await page.click("//a[normalize-space()='Headphones']");
 })
